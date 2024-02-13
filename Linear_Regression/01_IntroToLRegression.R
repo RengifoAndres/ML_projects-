f3eb658d9c427508406fe4f4ceab0f103e30d42c
Andres Rengifo <afrj1996@gmail.com>
2024-02-13 16:08:34 -0500

    load data in INTRO LINEAR Reg

--- Linear_Regression/01_IntroToLRegression.R
+++ Linear_Regression/01_IntroToLRegression.R
@@ -9,3 +9,13 @@ p_load(tidyverse,
       skim)
 
 
+### load the data
+
+## we are going to use a subsample for GEIH
+
+## load data
+db <- import("https://github.com/ignaciomsarmiento/datasets/blob/main/GEIH_sample1.Rds?raw=true")
+db <- as_tibble(db) %>% rename(gender=sex) 
+
+
+
