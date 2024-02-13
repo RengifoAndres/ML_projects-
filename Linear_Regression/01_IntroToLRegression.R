07998a4b1b43eea6bb80fc893f7e85797b624c77
Andres Rengifo <afrj1996@gmail.com>
2024-02-13 16:39:24 -0500

    Update 01_IntroToLRegression.R

--- Linear_Regression/01_IntroToLRegression.R
+++ Linear_Regression/01_IntroToLRegression.R
@@ -27,3 +27,44 @@ db_of<- db %>%
   mutate(ofic_ingLab= ofic_ingLab/1000)
 
 db_of  %>% dplyr:: select(ocupation, gender, ofic_ingLab) %>% head(4)
+
+## merge with the original data set
+
+db_int <- db %>% inner_join(db_of, by=c("oficio", "gender"))
+
+# child indicator
+db_int <- db_int %>%
+  mutate(flag = ifelse(age <= 18, 1, 0))
+
+#  total number of children per household
+
+db_int <- db_int %>%
+  group_by(directorio, secuencia_p) %>%
+  mutate(nmenores = sum(flag)) %>%
+  dplyr::select(-flag) %>% 
+  ungroup()
+
+
+db_int %>% dplyr:: select(directorio, secuencia_p, age , nmenores ) %>% tail()
+
+db_int <- db_int %>%
+  mutate(H_Head = ifelse( p6050== 1, 1, 0))
+
+table(db_int$H_Head)
+
+
+
+
+features<- c("ofic_ingLab", "nmenores",  "H_Head",  "age",  "gender" )
+
+db_int<- db_int %>% drop_na(any_of(features))
+
+
+## Regression
+linear_model<- lm(totalHoursWorked ~ ofic_ingLab + nmenores  +  nmenores*gender + H_Head + age + gender, data=db_int  )
+summary(linear_model)
+
+
+
+
+
